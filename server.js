a54f0d08783fa3aa30d95425750981ab53c8775e
Laura <laurasimila95@gmail.com>
2026-04-24 17:23:55 +0300

    Add server time (CI/CD test)

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+    const now = new Date();
+
     res.send(`
         <h1>Laura assignment week 1</h1>
-        <p>Hello from Fly.io!</p>
+        <p>Hello from Fly.io! (CI/CD working)</p>
         <p>This is my simple Node.js app deployed to the internet.</p>
+        <p>Current server time: ${now.toISOString()}</p>
     `);
 });
 
